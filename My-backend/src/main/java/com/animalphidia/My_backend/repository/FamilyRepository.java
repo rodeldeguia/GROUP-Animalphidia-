9f181f4febe3f9ccc2139daba343023fd9adb8c1
jessalyn776 <jessalyntrogon@gmail.com>
2025-12-30 01:42:30 +0800

    Create FamilyRepository.java

---
+++ My-backend/src/main/java/com/animalphidia/My_backend/repository/FamilyRepository.java
@@ -0,0 +1,9 @@
+package com.animalphidia.My_backend.repository;
+
+import com.animalphidia.My_backend.model.Family;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FamilyRepository extends JpaRepository<Family, Integer> {
+}
